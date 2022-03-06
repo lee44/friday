2d90c7595b613693dfc4fc9354c94340d557aa64
lee44 <jlee7772@yahoo.com>
2022-03-06 12:55:39 -0800

    Logout Fixed

--- server/routes/auth/authRouter.js
+++ server/routes/auth/authRouter.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { login } from '../../controllers/loginController.js';
+import { logout } from '../../controllers/logoutController.js';
 import { register } from '../../controllers/registerController.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', logout);
 
 export default router;
