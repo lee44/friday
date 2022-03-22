ff04fdca4482c2e108460bc89fcea45c94922e40
Lee44 <jlee7772@yahoo.com>
2022-03-21 22:30:47 -0700

    Changed findQnique to findMany

--- server/prisma/migrations/20220322052919_init/migration.sql
+++ server/prisma/migrations/20220322052919_init/migration.sql
@@ -1,13 +1,10 @@
 -- CreateTable
-CREATE TABLE "User" (
+CREATE TABLE "Users" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "role" TEXT NOT NULL,
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
