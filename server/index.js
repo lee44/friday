4cdeebea3d62cc9548716f80a0d3d746054b30d4
Lee44 <jlee7772@yahoo.com>
2022-03-21 23:51:19 -0700

    Adding allow oriign

--- server/index.js
+++ server/index.js
@@ -7,16 +7,8 @@ import userRouter from './routes/user/userRouter.js';
 
 const app = express();
 app.use(express.json());
-app.use(cors());
-
+app.use(cors({ credentials: true }));
 app.use(cookieParser());
-
-app.use((req, res, next) => {
-	res.append('Access-Control-Allow-Origin', ['*']);
-	res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.append('Access-Control-Allow-Headers', 'Content-Type');
-	next();
-});
 
 // Routes all authentication routes
 app.use('/api/auth', authRouter);
