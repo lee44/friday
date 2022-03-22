f06797c70fae0bb8336e66694e80b20032321937
Lee44 <jlee7772@yahoo.com>
2022-03-21 19:04:10 -0700

    Adjusted enpoint to heroku

--- client/src/config/Endpoints.ts
+++ client/src/config/Endpoints.ts
@@ -1,11 +1,11 @@
 export enum ENDPOINTS {
-    BASE_URL='https://git.heroku.com/friday-crud.git',
-    LOGIN='https://git.heroku.com/friday-crud.git/api/auth/login',
-    LOGOUT='https://git.heroku.com/friday-crud.git/api/auth/logout',
-    REGISTER='https://git.heroku.com/friday-crud.git/api/auth/register',
-    REFRESH_TOKEN='https://git.heroku.com/friday-crud.git/api/refreshtoken',
-    FETCH_USER='https://git.heroku.com/friday-crud.git/api/user',
-    FETCH_USERS='https://git.heroku.com/friday-crud.git/api/users',
-    DELETE_USER='https://git.heroku.com/friday-crud.git/api/user',
-    UPDATE_USER='https://git.heroku.com/friday-crud.git/api/user'
+    BASE_URL='https://friday-crud.herokuapp.com/',
+    LOGIN='https://friday-crud.herokuapp.com/api/auth/login',
+    LOGOUT='https://friday-crud.herokuapp.com/api/auth/logout',
+    REGISTER='https://friday-crud.herokuapp.com/api/auth/register',
+    REFRESH_TOKEN='https://friday-crud.herokuapp.com/api/refreshtoken',
+    FETCH_USER='https://friday-crud.herokuapp.com/api/user',
+    FETCH_USERS='https://friday-crud.herokuapp.com/api/users',
+    DELETE_USER='https://friday-crud.herokuapp.com/api/user',
+    UPDATE_USER='https://friday-crud.herokuapp.com/api/user'
 }
